3105608a620ee4865218a58a5e55ba360d503071
SunSnakeJack <sufeeyan.tale@northbkk.ac.th>
2024-08-30 20:33:09 +0700

    second commit

--- src/Login.js
+++ src/Login.js
@@ -1,87 +1,184 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+// import Avatar from '@mui/material/Avatar';
+// import Button from '@mui/material/Button';
+// import CssBaseline from '@mui/material/CssBaseline';
+// import TextField from '@mui/material/TextField';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+// import Checkbox from '@mui/material/Checkbox';
+// import Link from '@mui/material/Link';
+// import Grid from '@mui/material/Grid';
+// import Box from '@mui/material/Box';
+// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+// import Typography from '@mui/material/Typography';
+// import { makeStyles } from '@mui/styles';
+// import Container from '@mui/material/Container';
+
+
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
 
+// const useStyles = makeStyles((theme) => ({
+//   paper: {
+//     marginTop: theme.spacing(8),
+//     display: 'flex',
+//     flexDirection: 'column',
+//     alignItems: 'center',
+//   },
+//   avatar: {
+//     margin: theme.spacing(1),
+//     backgroundColor: theme.palette.secondary.main,
+//   },
+//   form: {
+//     width: '100%', // Fix IE 11 issue.
+//     marginTop: theme.spacing(1),
+//   },
+//   submit: {
+//     margin: theme.spacing(3, 0, 2),
+//   },
+// }));
 function Login() {
-    const navigate = useNavigate()
-    const MySwal = withReactContent(Swal)
+  const navigate = useNavigate()
+  const MySwal = withReactContent(Swal)
 
-    const [inputs, setInputs] = useState({});
+  const [inputs, setInputs] = useState({});
 
-    const handleChange = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setInputs(values => ({ ...values, [name]: value }))
-    }
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({ ...values, [name]: value }))
+  }
+  // const classes = useStyles();
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
-        const myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
+    const raw = JSON.stringify({
+      "username": inputs.username,
+      "password": inputs.password,
+      "expiresIn": 600000
+    });
 
-        const raw = JSON.stringify({
-            "username": inputs.username,
-            "password": inputs.password,
-            "expiresIn": 600000
-        });
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow"
+    };
 
-        const requestOptions = {
-            method: "POST",
-            headers: myHeaders,
-            body: raw,
-            redirect: "follow"
-        };
+    fetch("https://www.melivecode.com/api/login", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result)
+        if (result.status === 'ok') {
+          MySwal.fire({
+            html: <i>{result.message}</i>,
+            icon: 'success'
+          }).then((value) => {
+            localStorage.setItem('token', result.accessToken)
+            navigate('/profile')
+          })
+        } else {
+          MySwal.fire({
+            html: <i>{result.message}</i>,
+            icon: 'error'
+          })
+        }
+      })
+      .catch((error) => console.error(error));
 
-        fetch("https://www.melivecode.com/api/login", requestOptions)
-            .then((response) => response.json())
-            .then((result) => {
-                console.log(result)
-                if (result.status === 'ok') {
-                    MySwal.fire({
-                        html: <i>{result.message}</i>,
-                        icon: 'success'
-                    }).then((value) => {
-                        localStorage.setItem('token', result.accessToken)
-                        navigate('/profile')
-                    })
-                } else {
-                    MySwal.fire({
-                        html: <i>{result.message}</i>,
-                        icon: 'error'
-                    })
-                }
-            })
-            .catch((error) => console.error(error));
-        console.log(inputs);
-    }
+    console.log(inputs);
+  }
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label>Username:
-                    <input
-                        type="text"
-                        name="username"
-                        value={inputs.username || ""}
-                        onChange={handleChange}
-                    />
-                </label>
-                <label>Password:
-                    <input
-                        type="password"
-                        name="password"
-                        value={inputs.password || ""}
-                        onChange={handleChange}
-                    />
-                </label>
-                <input type="submit" />
-            </form>
-        <a href="register">Register</a>
+  return (
+    <div>
+      {/* <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
         </div>
-    )
+        <Box mt={8}>
+        </Box>
+      </Container> */}
+      <form onSubmit={handleSubmit}>
+        <label>Username:
+          <input
+            type="text"
+            name="username"
+            value={inputs.username || ""}
+            onChange={handleChange}
+          />
+        </label>
+        <label>Password:
+          <input
+            type="password"
+            name="password"
+            value={inputs.password || ""}
+            onChange={handleChange}
+          />
+        </label>
+        <input type="submit" />
+      </form>
+      <a href="register">Register</a>
+    </div>
+  )
 }
 
 export default Login
